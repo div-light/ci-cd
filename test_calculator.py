f097f02adad61ef7f27b58178fb519fc8e30abc1
iremkng <iremkng@iremkng.local>
2026-09-11 00:16:43 +0530

    Add CI workflow

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+# test_calculator.py
+import pytest
+
+from calculator import add, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(1, 0)
